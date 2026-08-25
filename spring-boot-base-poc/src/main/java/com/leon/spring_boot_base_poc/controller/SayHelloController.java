0e248e911dc1d9a058baf17873763bf0e46c3b11
leonnguyen-2004 <tandungnguyen918@gmail.com>
2026-08-25 22:33:32 +0700

    first API

---
+++ spring-boot-base-poc/src/main/java/com/leon/spring_boot_base_poc/controller/SayHelloController.java
@@ -0,0 +1,13 @@
+package com.leon.spring_boot_base_poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
